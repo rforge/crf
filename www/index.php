e9ee0b45d5dffd8709793c449c596dd561af1fd0
wulingyun <wulingyun@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-09-20 14:20:33 +0000

    Fix build error in R-Forge
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/crf@38 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -45,8 +45,8 @@ echo $contents; } ?>
 
 <p> <strong>Homepage:</strong> <a href="https://github.com/wulingyun/CRF">https://github.com/wulingyun/CRF</a>. </p>
 
-<p> <strong>Stable version:</strong> <a href="https://cran.r-project.org/web/packages/CRF/">CRAN</a>, 
-<a href="http://r-forge.r-project.org/projects/crf/">R-Forge</a>. </p>
+<p> <strong>Stable version:</strong> <a href="https://CRAN.R-project.org/package=CRF">CRAN</a>, 
+<a href="http://R-Forge.R-project.org/projects/crf/">R-Forge</a>. </p>
 
 <p> <strong>Latest beta version:</strong> <a href="https://github.com/wulingyun/CRF">GitHub</a>. </p>
 
